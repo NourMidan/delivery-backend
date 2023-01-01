7ad1a01b0495835e931235a2ef08ecad28faf6d4
unknown <nourmidan67@gmail.com>
2023-01-01 13:58:16 +0200

    port2

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  const port = process.env.PORT || 3000;
-
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
